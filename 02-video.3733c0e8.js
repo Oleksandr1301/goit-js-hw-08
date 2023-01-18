5b7aa88f774d298660068e5fe64f63e8348bb519
Oleksandr1301 <101977209+Oleksandr1301@users.noreply.github.com>
2023-01-18 16:07:41 +0000

    Deploying to gh-pages from @ Oleksandr1301/goit-js-hw-08@0dfbaa172724a87bad4d358f2a579cb7a67745ee 🚀

---
+++ 02-video.3733c0e8.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));
+//# sourceMappingURL=02-video.3733c0e8.js.map
