169eb5ace25e802fa2a90724912f12b58a01496a
Oleksandr1301 <viss82@ukr.net>
2023-01-18 17:11:28 +0200

    add start

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+const paletContainer = document.querySelector(".gallery");
+const imgMarkup = createImgGalleryMarkup(galleryItems);
+
+paletContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+// console.log(createImgGalleryMarkup(galleryItems));
+function createImgGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt"
+});
